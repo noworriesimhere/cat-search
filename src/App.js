44dd3a309b3c0bf5d0d606d4959a63e7f3fadec8
Alan Tran <contactalantran@gmail.com>
2020-12-07 11:11:55 -0500

    handleChange moved to own arrow function

--- src/App.js
+++ src/App.js
@@ -12,12 +12,17 @@ class App extends Component {
       monsters: [],
       searchField: ''
     };
+
   }
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then(users => this.setState({ monsters: users }));
+  }
+
+  handleChange = e => {
+    this.setState({ searchField: e.target.value })
   }
 
   render() {
@@ -29,7 +34,7 @@ class App extends Component {
       <div className="App">
         <SearchBox 
         placeholder='search cat'
-        handleChange={e => this.setState({ searchField: e.target.value })}
+        handleChange={this.handleChange}
         />
         <CardList monsters={filteredMonsters} />
       </div>
